f5d0252dea6894c9f6e730a1b6f331ed5a40e3ee
kojandy <kojandy@gmail.com>
2016-10-24 04:51:44 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,16 @@
-rm -f ~/.vimrc
+platform=$(uname)
+
+mkdir -p ~/dotfiles/dotfiles_old
+mv -f ~/.vimrc ~/dotfiles/dotfiles_old/.vimrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
+
+if [ $platform = 'Linux' ]
+then
+    sudo apt install vim
+    sudo apt install zsh
+fi
+chsh -s $(which zsh)
+if [ ! -d ~/.oh-my-zsh ]
+then
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
