eeeefbcc323e7ebd3e5284713ea49eca45dcf7a1
Ohjun Kwon <kojandy@gmail.com>
2016-10-28 18:19:14 +0900

    Add tmux plugin manager

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -1,2 +1,7 @@
 mv -f ~/.tmux.conf ~/dotfiles/dotfiles_old/tmux.conf
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+if [ ! -d ~/.tmux/plugins/tpm ]
+then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
