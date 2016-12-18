0d3fb7bee7fad58c499757f9e6ec50a3fda6e75b
Ohjun Kwon <kojandy@gmail.com>
2016-12-19 01:33:03 +0900

    Fix setup/zsh.sh

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -6,7 +6,7 @@ fi
 mv -f ~/.zshrc ~/.dotfiles/dotfiles_old/zshrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 mkdir -p ~/.oh-my-zsh/custom/themes
-mv -f ~/.oh-my-zsh/custom/themes ~/.dotfiles/oh-my-zsh/custom/themes
+mv -f ~/.oh-my-zsh/custom/themes ~/.dotfiles/dotfiles_old/oh-my-zsh/custom/themes
 ln -s ~/.dotfiles/oh-my-zsh/custom/themes/* ~/.oh-my-zsh/custom/themes
-mv -f ~/.oh-my-zsh/custom/alias.zsh ~/.dotfiles/oh-my-zsh/custom/alias.zsh
+mv -f ~/.oh-my-zsh/custom/alias.zsh ~/.dotfiles/dotfiles_old/oh-my-zsh/custom/alias.zsh
 ln -s ~/.dotfiles/oh-my-zsh/custom/alias.zsh ~/.oh-my-zsh/custom/alias.zsh
