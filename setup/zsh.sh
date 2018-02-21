59739d47b21a6e2b78458592554f5c88a6887796
Ohjun Kwon <kojandy@gmail.com>
2018-02-21 15:21:33 +0900

    Set ZSH_CUSTOM path

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -9,15 +9,3 @@ then
     mv -f ~/.zshrc ~/.dotfiles/dotfiles_old/zshrc
 fi
 ln -s ~/.dotfiles/zshrc ~/.zshrc
-mkdir -p ~/.oh-my-zsh/custom/themes
-mkdir -p ~/.dotfiles/dotfiles_old/oh-my-zsh/custom/themes
-if [ -e ~/.oh-my-zsh/custom/themes ]
-then
-    mv -f ~/.oh-my-zsh/custom/themes ~/.dotfiles/dotfiles_old/oh-my-zsh/custom/themes
-fi
-ln -s ~/.dotfiles/oh-my-zsh/custom/themes/ ~/.oh-my-zsh/custom
-if [ -e ~/.oh-my-zsh/custom/alias.zsh ]
-then
-    mv -f ~/.oh-my-zsh/custom/alias.zsh ~/.dotfiles/dotfiles_old/oh-my-zsh/custom/alias.zsh
-fi
-ln -s ~/.dotfiles/oh-my-zsh/custom/alias.zsh ~/.oh-my-zsh/custom/alias.zsh
