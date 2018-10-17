124325614fd1e95a85902f9be451ca752bca84f8
Ohjun Kwon <dev@kojandy.com>
2018-10-17 19:22:30 +0000

    Make link to .gitignore_global

--- setup/git.sh
+++ setup/git.sh
@@ -4,3 +4,9 @@ then
     mv -f ~/.gitconfig ~/.dotfiles/_old/gitconfig
 fi
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+
+if [ -e ~/.gitignore_global ]
+then
+    mv -f ~/.gitignore_global ~/.dotfiles/_old/gitignore_global
+fi
+ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
