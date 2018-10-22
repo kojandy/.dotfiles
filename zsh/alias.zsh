e319623734156c4f0d900414e6908218b0451163
Ohjun Kwon <dev@kojandy.com>
2018-10-22 01:37:51 +0000

    SO CUTE

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -8,7 +8,7 @@ alias vi='vim'
 alias venv='source venv/bin/activate'
 alias mkvenv='python3 -m venv venv'
 
-# common typos
+# i am so cuet
 alias claer='clear'
 alias clera='clear'
 alias celar='clear'
