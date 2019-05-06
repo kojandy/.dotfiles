c897ebae5cf8f9a0ecdf704f4c61a71da00d722d
Ohjun Kwon <dev@kojandy.com>
2019-05-07 06:49:16 +0900

    Add playerctl info in polybar

---
+++ polybar/playerctl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+player_status=$(playerctl status 2> /dev/null)
+
+if [ "$player_status" = "Playing" ]; then
+    echo " $(playerctl metadata title)"
+elif [ "$player_status" = "Paused" ]; then
+    echo " $(playerctl metadata title)"
+else
+    echo ""
+fi
