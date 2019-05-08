8266dd19403cda27f7cd67e109916bf16260510e
Ohjun Kwon <dev@kojandy.com>
2019-05-09 03:54:49 +0900

    Show artist info if set

--- polybar/playerctl.sh
+++ polybar/playerctl.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 player_status=$(playerctl status 2> /dev/null)
+title=$(playerctl metadata title 2> /dev/null)
+artist=$(playerctl metadata artist 2> /dev/null)
+
+if [ ! "$artist" = "" ]; then
+    title=$artist' - '$title
+fi
 
 if [ "$player_status" = "Playing" ]; then
-    echo " $(playerctl metadata title)"
+    echo " ""$title"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl metadata title)"
+    echo " ""$title"
 else
     echo ""
 fi
