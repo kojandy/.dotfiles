386d489b0e49f65e84388a610906a4ae3209bdff
Ohjun Kwon <dev@kojandy.com>
2019-05-19 09:38:36 +0900

    Add autojump-ranger integration

---
+++ ranger/plugins/autojump.py
@@ -0,0 +1,17 @@
+import subprocess
+
+from ranger.api.commands import Command
+
+
+class j(Command):
+    """
+    :j <directory>
+
+    Uses autojump to set the current directory.
+    """
+
+    def execute(self):
+        directory = subprocess.check_output(["autojump", self.arg(1)])
+        directory = directory.decode("utf-8", "ignore")
+        directory = directory.rstrip('\n')
+        self.fm.execute_console("cd " + directory)
