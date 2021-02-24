5cb5ef642d366edf73fe4ee7fd8a7009a70c0c84
Ohjun Kwon <dev@kojandy.com>
2021-02-24 14:55:28 +0900

    Ensure lua treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"javascript", "typescript", "rust", "python"},
+    ensure_installed = {"javascript", "typescript", "rust", "python", "lua"},
     highlight = {enable = true},
     incremental_selection = {
         enable = true,
