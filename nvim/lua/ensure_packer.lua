8c0aca8635df2d9119c7291635c1e7cada16fd67
Ohjun Kwon <dev@kojandy.com>
2021-03-27 10:49:56 +0900

    Quit vim after bootstrapping packer.nvim

--- nvim/lua/ensure_packer.lua
+++ nvim/lua/ensure_packer.lua
@@ -3,5 +3,6 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     vim.cmd('echo "Installing packer.nvim..."')
     vim.cmd('silent !git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
     local packer = require('plugins')
+    vim.cmd('autocmd User PackerComplete qa')
     packer.sync()
 end
