c490fa80490a4e62929c2277dfcd3f107ce2c9a3
Ohjun Kwon <dev@kojandy.com>
2024-05-29 18:19:20 +0900

    Add java path env in work.nix

--- hosts/work.nix
+++ hosts/work.nix
@@ -23,4 +23,10 @@
       "temurin@17"
     ];
   };
+
+  environment.variables = {
+    JAVA_8_HOME = "/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home";
+    JAVA_11_HOME = "/Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home";
+    JAVA_17_HOME = "/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home";
+  };
 }
