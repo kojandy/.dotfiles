388ef3a7149725dfdf3ed0ac9cb7245f13d33cbe
Ohjun Kwon <dev@kojandy.com>
2025-02-27 11:09:36 +0900

    feat(darwin): add hotkey settings

--- modules/darwin/preferences.nix
+++ modules/darwin/preferences.nix
@@ -33,6 +33,20 @@
         "size-immutable" = true;
         "contents-immutable" = true;
       };
+      "com.apple.symbolichotkeys" = {
+        AppleSymbolicHotKeys = {
+          "60" = { # 이전 입력 소스 선택
+            enabled = true;
+            value = {
+              parameters = [32 49 1048576];
+              type = "standard";
+            };
+          };
+          "61".enabled = false; # 입력 메뉴에서 다음 소스 선택
+          "64".enabled = false; # Spotlight 검색 보기
+          "65".enabled = false; # Finder 검색 윈도우 보기
+        };
+      };
     };
   };
 }
