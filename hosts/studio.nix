17bc23c5487534a66ee75f7bbb7adbd455b1306c
Ohjun Kwon <dev@kojandy.com>
2025-05-12 23:02:37 +0900

    feat(studio): move jj config file to xdg config

--- hosts/studio.nix
+++ hosts/studio.nix
@@ -17,7 +17,6 @@
     home.file = {
       ".profile".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/profile";
       ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/zsh/zshrc";
-      "Library/Application Support/jj".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/jj";
       ".dotfiles/bin/littlesnitch".source = config.lib.file.mkOutOfStoreSymlink "/Applications/Little Snitch.app/Contents/Components/littlesnitch";
       "Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home".source = "${pkgs.zulu8}";
       "Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home".source = "${pkgs.temurin-bin-11}";
@@ -27,6 +26,7 @@
       "broot".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/broot";
       "git".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/git";
       "ideavim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/ideavim";
+      "jj".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/jj";
       "kitty".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/kitty";
       "lf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/lf";
       "nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/nvim";
