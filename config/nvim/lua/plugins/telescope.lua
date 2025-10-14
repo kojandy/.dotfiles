e3ad7ad46d8a8db959737b6c628ce9be722b880f
Ohjun Kwon <dev@kojandy.com>
2025-10-15 02:47:33 +0900

    feat(vim): update telescope.nvim

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   {'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
+    branch = '0.1.x',
     dependencies = {'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzy-native.nvim'},
     cmd = 'Telescope',
     keys = {
