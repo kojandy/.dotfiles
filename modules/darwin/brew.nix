03408ba8d3f61a4c31c94051d0cc50c362f751b8
Ohjun Kwon <dev@kojandy.com>
2025-10-21 17:20:51 +0900

    fix: remove homebrew/services tap

--- modules/darwin/brew.nix
+++ modules/darwin/brew.nix
@@ -46,7 +46,6 @@ _: {
     onActivation.cleanup = "zap";
     onActivation.extraFlags = ["--quiet"];
     caskArgs.no_quarantine = true;
-    taps = [ "homebrew/services" ];
   };
 
   environment.variables.HOMEBREW_AUTO_UPDATE_SECS = "86400";
